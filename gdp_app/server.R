70e06ac96aa1974774080a0f6d5c3d16a48601ff
R Pranith Kumar <praneeth970@gmail.com>
2017-06-14 23:12:02 +0530

    Update server.R

--- gdp_app/server.R
+++ gdp_app/server.R
@@ -1,8 +1,14 @@
 source("gdp.R")
 
 shinyServer(function(input, output) {
-  output$gdpPlot<-renderPlot({
+  output$gdpPlot<-renderPlotly({
     df<-get.year(input$year)
-    barplot(df$Value,names.arg = df$Country.Code,las=2)
+    p <- plot_ly(
+      x = df$Country.Code,
+      y = df$Value,
+      name = "GDP",
+      text = df$Country.Name,
+      type = "bar"
+    )
   })
 })
